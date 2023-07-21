8ef80fddb3aa3899e2d4b9c7ecffefe83ec14b7e
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-21 19:19:49 +0300

    11.9 own Charizard Cypress tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,21 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  describe('Charizard Page', function () {
+    it('can be navigated from index', function () {
+      // for some reason visiting pokemon page doesn't work
+      // cy.visit('http://localhost:5000/pokemon/charizard')
+      cy.visit('http://localhost:5000')
+      cy.contains('charizard').click()
+      cy.contains('charizard')
+    })
+    it('contains the correct abilites', function () {
+      cy.visit('http://localhost:5000')
+      cy.contains('charizard').click()
+      cy.contains('charizard')
+      cy.get('.pokemon-abilities')
+        .should('contain', 'blaze')
+        .and('contain', 'solar power')
+    })
+  })
 })
