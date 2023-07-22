a55688a29d2745f4c28b353b7cfa3e169fc2d1e3
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-22 18:49:35 +0300

    11.15 increment the /version number

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/version', (req, res) => {
-  res.send('1.0.5') // change to ensure new version deploy
+  res.send('1.0.6') // change to ensure new version deploy
 })
 
 app.get('/health', (req, res) => {
