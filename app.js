4a364cc0e4ee7e2a5130f07d2d5a8b66b5d8cf61
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-24 15:50:38 +0300

    11.18 fix lint, simulate build error

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.get('/health', (req, res) => {
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
-
-// eslint-disable-next-line no-console
